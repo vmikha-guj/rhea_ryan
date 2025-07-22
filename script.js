eb8e042cf9491a2c5cca53c6e6621cac5fd98f90
vmikha-guj <vmikha@gmail.com>
2025-07-22 10:06:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -21,4 +21,42 @@ async function findTable() {
   resultDiv.style.color = "#333";
 
   try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbydqHDgOoL2iPHu0sbEBeR7gdK_bq9pAuWWQbRSGx4s1kfEtZ-CbJX68-lAU7usHLon/exec?nam_
+    const response = await fetch('https://script.google.com/macros/s/AKfycbydqHDgOoL2iPHu0sbEBeR7gdK_bq9pAuWWQbRSGx4s1kfEtZ-CbJX68-lAU7usHLon/exec?name=' + encodeURIComponent(name));
+    const rawText = await response.text();
+
+    // Convert line breaks for display
+    const htmlText = rawText.replace(/\r?\n/g, "<br>");
+    resultDiv.innerHTML = htmlText;
+
+    // Parse table numbers
+    const lines = rawText.split(/\r?\n/);
+    const foundTables = new Set();
+
+    for (let i = 0; i < lines.length; i++) {
+      const match = lines[i].match(/Table (\d+)/);
+      if (match) {
+        foundTables.add(parseInt(match[1]));
+      }
+    }
+
+    if (foundTables.size === 1) {
+      const tableNum = [...foundTables][0];
+      const table = tablePositions.find(t => t.table === tableNum);
+      if (table) {
+        const rect = layout.getBoundingClientRect();
+        const x = (table.x / 100) * rect.width;
+        const y = (table.y / 100) * rect.height;
+        highlight.style.left = `${x}px`;
+        highlight.style.top = `${y}px`;
+        highlight.style.display = "block";
+      }
+    } else {
+      highlight.style.display = "none";
+    }
+  } catch (error) {
+    console.error("Fetch error:", error);
+    resultDiv.innerHTML = "Error occurred.";
+    resultDiv.style.color = "red";
+    highlight.style.display = "none";
+  }
+}
